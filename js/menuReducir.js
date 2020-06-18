6f3da086bb6001a8968903c5d0b61527423bd9a2
Kewuin Asus <kwyate21@gmail.com>
2020-06-18 11:52:24 -0500

    Se agraga script para que oculte y muestre el inicio

--- js/menuReducir.js
+++ js/menuReducir.js
@@ -3,16 +3,24 @@ class Menu{
 		this.box_principal = document.querySelector(box_principal);
 		this.btn_ocultar = document.querySelector(btn_ocultar);
 		this.box_texto = document.querySelector(".box-texto");
+		this.btn_mostrar = document.querySelector("#btn_inicio");
+		this.box_informacion = document.querySelector(".box-informacion");
 	}
 	ocultar(){
 		this.btn_ocultar.addEventListener("click", (e)=>{
-			console.log("click ----")
-			this.box_principal.classList.remove("vh-100");
-			this.box_principal.classList.add("bg-img-minimizar");
-			// this.box_principal.classList.add("position-sticky");
+			this.box_principal.classList.replace("bg-img-maximizar", "bg-img-minimizar");
 			this.box_texto.classList.add("d-none");
+			this.box_informacion.classList.replace("d-none", "d-block");
+		})
+	}
+	expandir(){
+		this.btn_mostrar.addEventListener("click", ()=>{
+			this.box_principal.classList.replace("bg-img-minimizar","bg-img-maximizar");
+			this.box_texto.classList.remove("d-none");
+			this.box_informacion.classList.replace("d-block", "d-none");
 		})
 	}
 }
 const m = new Menu(".bg-img", ".btn-ocultar");
 m.ocultar();
+m.expandir();
